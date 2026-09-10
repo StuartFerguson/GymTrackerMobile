105b04017cc18d2bfebd2cb7dd5b34493061de69
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-09-10 17:06:44 +0100

    test: allow longer Android driver startup

--- tests/GymTrackerMobile.UI.AutomationTests/Infrastructure/AndroidDriverFixture.cs
+++ tests/GymTrackerMobile.UI.AutomationTests/Infrastructure/AndroidDriverFixture.cs
@@ -28,6 +28,9 @@ public sealed class AndroidDriverFixture : IAsyncLifetime
         options.AddAdditionalAppiumOption("appium:fullReset", false);
         options.AddAdditionalAppiumOption("appium:autoGrantPermissions", true);
         options.AddAdditionalAppiumOption("appium:optionalIntentArguments", "-e gymtracker.uiTestMode true");
+        options.AddAdditionalAppiumOption("appium:adbExecTimeout", 120_000);
+        options.AddAdditionalAppiumOption("appium:uiautomator2ServerInstallTimeout", 120_000);
+        options.AddAdditionalAppiumOption("appium:uiautomator2ServerLaunchTimeout", 120_000);
         Driver = new AndroidDriver(Settings.ServerUrl, options, TimeSpan.FromSeconds(90));
         Driver.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
         return Task.CompletedTask;
